1a643bbb6dabf50f478cf1d9259e87e4104d627b
Tim Beck <becktimd@gmail.com>
2025-02-04 22:37:19 -0800

    briscola on debug view, handle_info for score/redeal

--- lib/briscolino/games/game_server.ex
+++ lib/briscolino/games/game_server.ex
@@ -27,14 +27,6 @@ defmodule Briscolino.GameServer do
     GenServer.call(pid, {:play, card})
   end
 
-  def score(pid) do
-    GenServer.call(pid, :score)
-  end
-
-  def redeal(pid) do
-    GenServer.call(pid, :redeal)
-  end
-
   def state(pid) do
     GenServer.call(pid, :state)
   end
@@ -81,31 +73,31 @@ defmodule Briscolino.GameServer do
   end
 
   @impl true
-  def handle_call(:score, _from, state) do
-    case Briscola.Game.score_trick(state) do
-      {:error, err} ->
-        {:reply, {:error, err}, state}
-
-      {:ok, game, winner} ->
-        Process.send_after(self(), :redeal, 2000)
-        {:reply, {:ok, game, winner}, %ServerState{state | gamestate: game}}
-    end
-  end
-
-  @impl true
-  def handle_call(:redeal, _from, state) do
-    case Briscola.Game.redeal(state) do
-      {:error, err} -> {:reply, {:error, err}, state}
-      game -> {:reply, {:ok, game}, %ServerState{state | gamestate: game}}
-    end
-  end
-
-  @impl true
   def handle_call(:result, _from, state) do
     if Briscola.Game.game_over?(state.gamestate) do
       {:reply, Enum.sort_by(state.gamestate.players, &Briscola.Player.score(&1), :desc), state}
     else
       {:reply, nil, state}
+    end
+  end
+
+  @impl true
+  def handle_info(:score, state) do
+    case Briscola.Game.score_trick(state.gamestate) do
+      {:error, _err} ->
+        {:noreply, state}
+
+      {:ok, game, _winner} ->
+        Process.send_after(self(), :redeal, 2000)
+        {:noreply, %ServerState{state | gamestate: game}}
+    end
+  end
+
+  @impl true
+  def handle_info(:redeal, state) do
+    case Briscola.Game.redeal(state.gamestate) do
+      {:error, _err} -> {:noreply, state}
+      game -> {:noreply, %ServerState{state | gamestate: game}}
     end
   end
 end
