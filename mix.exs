64150f3b4eddbe9539d9d23cc3ca0540ae76c46f
Tim Beck <becktimd@gmail.com>
2025-02-08 15:14:48 -0800

    upgrade briscola dependency

--- mix.exs
+++ mix.exs
@@ -55,7 +55,7 @@ defmodule Briscolino.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:briscola, "~> 0.1.0"}
+      {:briscola, "~> 0.1.1"}
     ]
   end
 
