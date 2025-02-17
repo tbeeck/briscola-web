79ea4771161c2af27deedb131cecd97f6e572826
Tim Beck <socaltdb@gmail.com>
2025-02-16 16:21:16 -0800

    hook for game timer

---
+++ assets/js/GameTimer.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const GameTimer = {
+  mounted() {
+    this.start = 0;
+    this.end = 0;
+    this.handleEvent("timer", ({ deadline_start, deadline_end }) => {
+      this.start = deadline_start;
+      this.end = deadline_end;
+    });
+    let update = () => {
+      let percent = Math.min(
+        1 - (this.end - Date.now()) / (this.end - this.start),
+        1
+      ) * 100;
+      this.el.style.width = percent + "%";
+    };
+    setInterval(update, 20);
+  }
+}
+
+export default GameTimer;
