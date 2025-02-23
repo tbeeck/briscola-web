fb6f411251cbb3cb12fe494c8dc0fcccb5a70dc7
Tim Beck <becktimd@gmail.com>
2025-02-23 14:13:01 -0800

    lobby player list component

---
+++ lib/briscolino_web/components/live_game/lobby_components.ex
@@ -0,0 +1,52 @@
+defmodule BriscolinoWeb.LiveGame.LobbyComponents do
+  use Phoenix.Component
+  use Gettext, backend: BriscolinoWeb.Gettext
+
+  import BriscolinoWeb.CoreComponents
+
+  alias Briscolino.LobbyServer
+  alias Briscolino.LobbyServer.LobbyState
+
+  @doc """
+  Renders players in the sidebar, either for
+  a game or a lobby.
+  """
+  attr :lobby, LobbyState, required: false
+
+  def lobby_player_list(assigns) do
+    ~H"""
+    <ul class="pl-4 text-primary_text">
+      <%= for player <- @lobby.players do %>
+        <li class="rounded-md" ]}>
+          <div class="flex flex-row items-center p-2">
+            <div class="rounded-full w-12 h-12 m-2 mr-4 outline flex items-center justify-center">
+              Image
+            </div>
+            <div class="flex flex-col flex-grow">
+              <div class="text-lg">{player.name}</div>
+              <div class={["flex flex-row items-center"]}>
+                <div class="pl-4 text-md"></div>
+              </div>
+            </div>
+          </div>
+        </li>
+      <% end %>
+      <%= if !LobbyServer.lobby_full(@lobby) do %>
+        <%= for _ <- 0..(4-length(@lobby.players)-1) do %>
+          <div class="flex flex-row items-center p-2">
+            <div class="rounded-full w-12 h-12 m-2 mr-4 outline flex items-center justify-center">
+              <.icon name="hero-plus" class="w-[50%] h-[50%]" />
+            </div>
+            <div class="flex flex-col flex-grow">
+              <div class="text-lg">...</div>
+              <div class={["flex flex-row items-center"]}>
+                <div class="pl-4 text-md"></div>
+              </div>
+            </div>
+          </div>
+        <% end %>
+      <% end %>
+    </ul>
+    """
+  end
+end
