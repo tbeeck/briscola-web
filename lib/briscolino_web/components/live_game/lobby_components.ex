25d406031aee3e9c3707e73ae8c506c3a8cde188
Tim Beck <becktimd@gmail.com>
2025-03-17 22:17:35 -0700

    remove placeholder image from lobby list

--- lib/briscolino_web/components/live_game/lobby_components.ex
+++ lib/briscolino_web/components/live_game/lobby_components.ex
@@ -20,10 +20,6 @@ defmodule BriscolinoWeb.LiveGame.LobbyComponents do
       <%= for {player, idx} <- Enum.with_index(@lobby.players) do %>
         <li class="rounded-md" ]}>
           <div class="flex flex-row items-center p-2">
-            <div class="rounded-full w-12 h-12 m-2 mr-4 outline
-                        flex items-center justify-center">
-              Image
-            </div>
             <div class="flex flex-col flex-grow">
               <.player_name name={player.name} highlighted={@highlighted == idx} />
               <div class="flex flex-row items-center">
@@ -38,9 +34,6 @@ defmodule BriscolinoWeb.LiveGame.LobbyComponents do
       <%= if !LobbyServer.lobby_full(@lobby) do %>
         <%= for _ <- 0..(4-length(@lobby.players)-1) do %>
           <div class="flex flex-row items-center p-2">
-            <div class="rounded-full w-12 h-12 m-2 mr-4 outline flex items-center justify-center">
-              <.icon name="hero-plus" class="w-[50%] h-[50%]" />
-            </div>
             <div class="flex flex-col flex-grow">
               <div class="text-lg">...</div>
               <div class="flex flex-row items-center">
